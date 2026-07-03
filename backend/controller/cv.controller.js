c77b7cf0447ede2da739b40b534d4c38bcbfdab1
elshaddai tambuwun <elshatambuwun17@gmail.com>
2026-07-03 16:30:43 +0700

    change cv document in controller

--- backend/controller/cv.controller.js
+++ backend/controller/cv.controller.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 const myCV = (req, res) => {
-    const filePath = path.join(__dirname, "..", "files", "CV_Elsha_Tambuwun.pdf");
+    const filePath = path.join(__dirname, "..", "files", "Curiculum_Vitae_Elsha_Tambuwun.pdf");
 
-    res.download(filePath, "CV_Elsha_Tambuwun.pdf", (err) => {
+    res.download(filePath, "Curiculum_Vitae_Elsha_Tambuwun.pdf", (err) => {
         if (err) {
             console.log("Error download CV:", err);
             return res.status(500).send("File tidak ditemukan");
